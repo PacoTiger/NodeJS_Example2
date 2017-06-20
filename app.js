52e44f937b4c2c300672b044e63734c4d033bf53
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-06-20 11:03:02 +0100

    Getting started

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+io.on('connection', function (client) {
+  console.log('Client Connected.....');
+
+  // listen for answers here
+  client.on('answer', function (question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+
+
+
+  client.on('question', function (question) {
+  	if(!client.question_asked) {
+    	client.broadcast.emit('question', question);
+    	client.question_asked = true;
+	};
+  });
+});
+
+server.listen(3333);
